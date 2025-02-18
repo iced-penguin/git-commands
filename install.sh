5fb5c2054eeb2ab1cf6dec84b8a53fc622168926
iced-penguin <icedpenguin0504@gmail.com>
2025-02-19 00:40:16 +0900

    refactor: インデントを揃える & クオーティング

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for file in git-*; do
-    if which "$file" > /dev/null 2>&1; then
-        echo "$file is already installed. Skip."
-        continue
-    else
-        echo "Installing $file..."
-        chmod +x "$file"
-        ln -s $(pwd)/"$file" /usr/local/bin/"$file"
-    fi
+  if which "$file" >/dev/null 2>&1; then
+    echo "$file is already installed. Skip."
+    continue
+  else
+    echo "Installing $file..."
+    chmod +x "$file"
+    ln -s "$(pwd)/$file" /usr/local/bin/"$file"
+  fi
 done
